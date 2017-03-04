ec915a44a2ab889a3bdae1a56e8030406b4be534
Rico Tilgner <rico.tilgner@gmail.com>
2017-03-04 12:01:30 +0100

    Optimize relu in case of no negative slope

--- src/caffe/layers/relu_layer.cpp
+++ src/caffe/layers/relu_layer.cpp
@@ -11,10 +11,16 @@ void ReLULayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
   const int count = bottom[0]->count();
-  Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
-  for (int i = 0; i < count; ++i) {
-    top_data[i] = std::max(bottom_data[i], Dtype(0))
-        + negative_slope * std::min(bottom_data[i], Dtype(0));
+  if (this->layer_param_.relu_param().has_negative_slope()) {
+    Dtype negative_slope = this->layer_param_.relu_param().negative_slope();
+    for (int i = 0; i < count; ++i) {
+      top_data[i] = std::max(bottom_data[i], Dtype(0))
+          + negative_slope * std::min(bottom_data[i], Dtype(0));
+    }
+  } else {
+    for (int i = 0; i < count; ++i) {
+      top_data[i] = std::max(bottom_data[i], Dtype(0));
+    }
   }
 }
 
